4abe58fbcb7cdcd4cc1b4b7f6b375cc99b22a6d6
Youness CN8VX <cn8vx.ma@gmail.com>
2025-05-14 22:17:23 +0100

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Configuration file for EchoLink from Svxlink
+ */
+// Chemin des logs
+// Path to SVXLink logs
+$log_files = ['/var/log/svxlink','/var/log/svxlink.1'];
+
+// Path to SVXLink configuration file
+$svxlink_config = '/etc/svxlink/svxlink.conf';
+
+// Display the banner title.
+// Afficher le titre de la bannière.
+$TITLEBAN = ("Connection logs for EchoLink from Svxlink");
+
+// Define the logo path.
+// Définir le chemin du logo.
+define("LOGO_PATH", "img/M.A.R.R.I_trans.png");
+
+// Sysop Name, nom du sysop
+$SYSOP = ("CN8VX");
+
+// Refresh interval for the table (in seconds)
+$refresh_interval = 5;
+
+// login section
+$CONFIG = [
+    // Enable/disable the login function (true = enabled, false = disabled)
+    // Activer/désactiver la fonction de login (true = activé, false = désactivé)
+    'login_required' => true,
+    
+    // Show the logged in user and the logout button (true = show, false = hide)
+    // This setting only has an effect if login_required is enabled
+    // Afficher l'utilisateur connecté et le bouton de déconnexion (true = afficher, false = cacher)
+    // Ce paramètre n'a d'effet que si login_required est activé
+    'show_user_info' => true,
+    
+    // List of users ("username" => "password")
+    // These are just examples, you can put any "username" and any "password"
+    'users' => [
+        "admin" => "admin",
+        "user" => "123456",
+        "user1" => "user123"
+    ]
+];
+
+// If login_required is disabled, force show_user_info to false
+if (!$CONFIG['login_required']) {
+    $CONFIG['show_user_info'] = false;
+}
+?>
